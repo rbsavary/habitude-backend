195696cc74b99fabd37d5704a782708fb0a699d7
Steve McLaughlin <stevejrmc@gmail.com>
2023-03-29 13:46:26 +0100

    Posts model v1 complete; dynamic environment config and readme update

---
+++ loadenv.js
@@ -0,0 +1,26 @@
+import { config } from "dotenv";
+
+export default function Env() {
+  const env = config({
+    path: `./.env.${process.env.NODE_ENV || "development"}`
+  });
+
+  if (env.error) {
+    throw env.error;
+  }
+  const {
+    PORT,
+    DATABASE_TYPE,
+    DATABASE_NAME,
+    DATABASE_USER,
+    DATABASE_PASSWORD
+  } = env.parsed;
+  return {
+    server: {
+      port: parseInt(PORT, 10)
+    },
+    database: {
+      MONGODB_URI: DATABASE_TYPE + DATABASE_USER + DATABASE_PASSWORD + DATABASE_NAME
+    }
+  }
+}
---
+++ models/index.js
@@ -0,0 +1,3 @@
+import Posts from "./Posts.js";
+
+export { Posts };
