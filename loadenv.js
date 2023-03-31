c56ee2b525d12e0eb1af5170842af0e63d8321e2
Steve McLaughlin <stevejrmc@gmail.com>
2023-03-31 16:51:10 +0100

    amended path to load correct env variable

--- loadenv.js
+++ loadenv.js
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 
 export default function Env() {
   const env = config({
-    path: `./.env.${process.env.NODE_ENV}` || "production"
+    path: process.env.NODE_ENV === "development" ? `./.env.${process.env.NODE_ENV}` : "production"
   });
 
   if (env.error) {
